74ffd91a7fc81cee4cb78d4e24c98266594dacf4
mark7389 <mark.soliman73@gmail.com>
2017-12-27 12:19:09 -0500

    initial commit

---
+++ scripts/build.js
@@ -0,0 +1,7 @@
+require("child_process").spawn("npm",
+["run build"], 
+{
+    stdio:"inherit",
+    cwd: "client",
+    shell: true
+});
---
+++ scripts/start-client.js
@@ -0,0 +1,7 @@
+require("child_process").spawn("npm",
+["start"], 
+{
+    stdio:"inherit",
+    cwd: "client",
+    shell: true
+});
