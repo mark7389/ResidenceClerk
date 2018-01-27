faec927b1dbb1a053f5a54edf0973f7f8f984119
mark7389 <mark.soliman73@gmail.com>
2018-01-27 09:16:53 -0500

    heroku

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const app = require("express")();
+const app = express();
 const path = require("path");
 const bodyParser =  require("body-parser");
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 const session = require("express-session");
-const MongoClient = require('mongodb').MongoClient;
 const passport = require('./lib/passport');
 const proxy = require('express-http-proxy');
 
@@ -13,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static("./client/build"));
-
+app.use(routes);
 app.set('trust proxy', 1);
 app.use(session({secret: "beesnees", proxy:true, resave: true, saveUninitialized: true, cookie:{
       expires:new Date(Date.now()+14400000),
@@ -21,7 +20,7 @@ app.use(session({secret: "beesnees", proxy:true, resave: true, saveUninitialized
 }}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(routes);
+
 
 
 app.listen(PORT, function(){
